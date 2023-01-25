75abe60c6a0ca6255a9df8babee89d392a5d8120
pavel <pavels.judakov@inbox.lv>
2023-01-25 21:30:42 +0200

    Last_Update 25.01.23

---
+++ src/Task_10/ThreeDigits2.java
@@ -0,0 +1,32 @@
+package Task_10;
+
+import java.util.Scanner;
+
+public class ThreeDigits2 {
+
+    public double inputUser(String message) {
+        double userInput = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(message);
+        userInput = scanner.nextDouble();
+        return userInput;
+
+    }
+    public void printMsg(String message){
+        System.out.println(message);
+    }
+    public double compare(double parameter1, double parameter2, double parameter3 ){
+        double compareResult = 0;
+
+        if ((parameter1>=parameter2)&&(parameter1>=parameter3)) {
+            compareResult = parameter1;
+        } else  if ((parameter2>=parameter1)&&(parameter2>=parameter3)) {
+            compareResult = parameter2;
+        } else  {
+            compareResult = parameter3;
+        }
+
+        return compareResult;
+    }
+
+}
